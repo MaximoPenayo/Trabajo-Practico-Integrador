0c49a97e4bca32fc6eb86384402c6ec998b72c21
Maximo Joel Penayo <141769168+MaximoPenayo@users.noreply.github.com>
2025-06-18 19:53:14 -0300

    Stock

--- main.c
+++ main.c
@@ -46,3 +46,15 @@ Venta registrarVenta(producto productos[], int n, Venta* listaVentas);
 void guardarVentasArchivo(Venta* lista, producto productos[], const char* nombreArchivo);
 void mostrarVentas(Venta* lista);
 float calcularTotal(Venta* lista);
+
+producto productos[MAX_PRODUCTOS] = {
+    {1, "Lata de Pepsi", 1400.0, 20},
+    {2, "Coca Cola", 3900.0, 15},
+    {3, "Pancho", 1000.0, 30},
+    {4, "Pebete", 1500.0, 20},
+    {5, "Hamburguesa", 4000.0, 10},
+    {6, "Alfajor Oreo", 1500.0, 30},
+    {7, "Alfajor Milka", 1500.0, 30},
+    {8, "Bon o Bon", 1100.0, 20},
+    {9, "Empanadas", 600.0, 12}
+};
